f186b8a595d2c65088b4ad98c25bdfb9f836a78d
Elliott Linder <elliott.darfink@gmail.com>
2019-10-31 13:23:23 +0100

    test: add 'node' count validation

--- TrieMap.test.lua
+++ TrieMap.test.lua
@@ -8,12 +8,16 @@ function sample_trie()
   return trie
 end
 
-function iter_to_array(iterator)
-  local array = {}
-  for value in iterator do
-    array[#array + 1] = value
+function iter_to_table(iterator)
+  local result = {}
+  for key, value in iterator do
+    if value == nil then
+      result[#result + 1] = key
+    else
+      result[key] = value
+    end
   end
-  return array
+  return result
 end
 
 describe('TrieMap', function()
@@ -46,11 +50,29 @@ describe('TrieMap', function()
 
     assert.same(trie:remove('toast'), 3)
     assert.same(trie:get('toast'), nil)
+    assert.same(trie:nodes(), 4)
     assert.same(trie:len(), 2)
+  end)
 
-    trie:remove('team')
-    trie:remove('test')
-    assert.truthy(trie:is_empty())
+  it('should be possible to remove intermediate nodes', function()
+    local trie = TrieMap.new()
+    trie:insert('test', 1)
+    trie:insert('te', 2)
+
+    -- Remove an intermediate node with a single child
+    trie:remove('te')
+
+    assert.same(trie:nodes(), 2)
+    assert.same(trie:len(), 1)
+
+    trie:insert('te', 2)
+    trie:insert('team', 3)
+
+    -- Remove an intermediate node with multiple children
+    trie:remove('te')
+
+    assert.same(trie:nodes(), 4)
+    assert.same(trie:len(), 2)
   end)
 
   it('should be possible to iterate values without a prefix', function()
@@ -68,8 +90,8 @@ describe('TrieMap', function()
     trie:insert('barfoo', 5)
     trie:insert('barcar', 6)
 
-    local values = iter_to_array(trie:values('barca'))
-    assert.same(values, {4, 6})
+    local values = iter_to_table(trie:values('barca'))
+    assert.same(values, { 6, 4 })
   end)
 
   it('should store keys in lexicographic order', function()
@@ -77,7 +99,34 @@ describe('TrieMap', function()
     trie:insert('z', 1)
     trie:insert('a', 2)
 
-    local keys = iter_to_array(trie:keys())
-    assert.same(keys, {'a', 'z'})
+    local keys = iter_to_table(trie:keys())
+    assert.same(keys, { 'a', 'z' })
+
+    trie:clear()
+    trie:insert('test', 1)
+    trie:insert('team', 2)
+
+    local keys = iter_to_table(trie:keys())
+    assert.same(keys, { 'team', 'test' })
+  end)
+
+  it('should update size when inserting a value that replaces an intermediate node', function()
+    local trie = TrieMap.new()
+
+    -- Insert keys to create an intermediate node with label 'te'
+    trie:insert('test', 1)
+    trie:insert('team', 2)
+
+    -- Insert a key with the same label as the intermediate node
+    assert.falsy(trie:insert('te', 3))
+    assert.same(trie:len(), 3)
+  end)
+
+  it('should create an intermediate node if a key is a complete prefix of an existing label', function()
+    local trie = TrieMap.new()
+    trie:insert('test', 1)
+    trie:insert('te', 2)
+    assert.same(trie:nodes(), 3)
+    assert.same(trie:len(), 2)
   end)
 end)
